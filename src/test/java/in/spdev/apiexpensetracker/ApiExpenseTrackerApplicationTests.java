f2ab0928b9818952a0b4eda6365c0f956c8d0208
techAnshumaan <tech.spmaan@gmail.com>
2023-05-18 09:17:40 +0530

    test

---
+++ src/test/java/in/spdev/apiexpensetracker/ApiExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.spdev.apiexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExpenseTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
